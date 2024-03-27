4d703b0f22c6a2cca46f522d8a06323d2e791f3c
mangala-karunarathne <mangalaicc@gmail.com>
2024-03-27 22:37:58 +0530

    CHG | Get files and Get list of files

--- upload_files.js
+++ upload_files.js
@@ -3,8 +3,8 @@ import fs from "fs";
 
 async function main(){
     const file_upload_response = await openai.files.create({
-        file : fs.createReadStream("assistant_queries.txt"),
-        purpose: "assistants"
+        file : fs.createReadStream("spanish_translated_sample_file.jsonl"), // change file name here to upload
+        purpose: "fine-tune"
     })
 
     console.log(file_upload_response);
